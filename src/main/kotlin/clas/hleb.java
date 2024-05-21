d3c5dc2d4c4b312222456a79b6b023c8c7da8da1
Maxim Igumnov <161121113+MaximHoLyMaxim@users.noreply.github.com>
2024-05-21 23:28:03 +0300

    hleb.java

---
+++ src/main/kotlin/clas/hleb.java
@@ -0,0 +1,10 @@
+package clas;
+public class hleb {
+    val name = "БурПром"
+    val kategori = "Мучное"
+    var cena = 35
+    var kolichestvo = 20
+    var stopimostHleb = cena*kolichestvo
+    println("Характеристика хлеба: \n")
+    println("Название: $name, Категория: $kategori, Цена: $cena , Количество на складе: $kolichestvo")
+}
