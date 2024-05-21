0e7dba0612260756451bdd02af97cb74a4a7643f
Maxim Igumnov <161121113+MaximHoLyMaxim@users.noreply.github.com>
2024-05-21 23:28:58 +0300

    milk.java

---
+++ src/main/kotlin/clas/milk.java
@@ -0,0 +1,10 @@
+package clas;
+public class milk {
+    val name = "Домик в деревне"
+    val kategori = "Нормализованное"
+    var cena = 100
+    var kolichestvo = 55
+    var stopimostMilk = cena*kolichestvo
+    println("Характеристика молока: \n")
+    println("Название: $name, Категория: $kategori, Цена: $cena , Количество на складе: $kolichestvo")
+}
